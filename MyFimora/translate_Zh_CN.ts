a585b07b680800822c4d0890212eb91b68d67761
LuckyKing <wangbiao@wondershare.cn>
2017-06-08 15:45:56 +0800

    添加资源文件和样式表文件

---
+++ MyFimora/translate_Zh_CN.ts
@@ -0,0 +1,256 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>FilmoraToolbar</name>
+    <message>
+        <location filename="filmoratoolbar.cpp" line="70"/>
+        <source>FILE</source>
+        <translation type="unfinished">文件</translation>
+    </message>
+    <message>
+        <location filename="filmoratoolbar.cpp" line="71"/>
+        <source>Exit</source>
+        <translation type="unfinished">退出</translation>
+    </message>
+    <message>
+        <location filename="filmoratoolbar.cpp" line="73"/>
+        <source>Edit</source>
+        <translation type="unfinished">编辑</translation>
+    </message>
+    <message>
+        <location filename="filmoratoolbar.cpp" line="74"/>
+        <source>copy</source>
+        <translation type="unfinished">拷贝</translation>
+    </message>
+    <message>
+        <location filename="filmoratoolbar.cpp" line="75"/>
+        <source>paste</source>
+        <translation type="unfinished">粘贴</translation>
+    </message>
+    <message>
+        <location filename="filmoratoolbar.cpp" line="76"/>
+        <source>cut</source>
+        <translation type="unfinished">剪切</translation>
+    </message>
+    <message>
+        <location filename="filmoratoolbar.cpp" line="78"/>
+        <source>Export</source>
+        <translation type="unfinished">导出</translation>
+    </message>
+    <message>
+        <location filename="filmoratoolbar.cpp" line="79"/>
+        <location filename="filmoratoolbar.cpp" line="84"/>
+        <location filename="filmoratoolbar.cpp" line="89"/>
+        <source>1</source>
+        <translation type="unfinished">1</translation>
+    </message>
+    <message>
+        <location filename="filmoratoolbar.cpp" line="80"/>
+        <location filename="filmoratoolbar.cpp" line="85"/>
+        <location filename="filmoratoolbar.cpp" line="90"/>
+        <source>2</source>
+        <translation type="unfinished">2</translation>
+    </message>
+    <message>
+        <location filename="filmoratoolbar.cpp" line="81"/>
+        <location filename="filmoratoolbar.cpp" line="86"/>
+        <location filename="filmoratoolbar.cpp" line="91"/>
+        <source>3</source>
+        <translation type="unfinished">3</translation>
+    </message>
+    <message>
+        <location filename="filmoratoolbar.cpp" line="83"/>
+        <source>Register</source>
+        <translation type="unfinished">注册</translation>
+    </message>
+    <message>
+        <location filename="filmoratoolbar.cpp" line="88"/>
+        <source>Help</source>
+        <translation type="unfinished">帮助</translation>
+    </message>
+    <message>
+        <location filename="filmoratoolbar.cpp" line="95"/>
+        <source>MYFIMORA_SKIN</source>
+        <translation type="unfinished">皮肤</translation>
+    </message>
+    <message>
+        <location filename="filmoratoolbar.cpp" line="97"/>
+        <source>skin</source>
+        <translation type="unfinished">皮肤</translation>
+    </message>
+    <message>
+        <location filename="filmoratoolbar.cpp" line="98"/>
+        <source>click to change window skin</source>
+        <translation type="unfinished">点击这里来改变皮肤吧。。。</translation>
+    </message>
+</context>
+<context>
+    <name>PropertySettingCtl</name>
+    <message>
+        <location filename="propertysettingctl.cpp" line="19"/>
+        <source>English</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="propertysettingctl.cpp" line="20"/>
+        <source>Chinese</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PropertySettingWnd</name>
+    <message>
+        <location filename="propertysettingwnd.cpp" line="38"/>
+        <location filename="propertysettingwnd.cpp" line="254"/>
+        <source>YouTube temporary folder</source>
+        <translation>YouTu 临时文件夹</translation>
+    </message>
+    <message>
+        <location filename="propertysettingwnd.cpp" line="44"/>
+        <location filename="propertysettingwnd.cpp" line="55"/>
+        <source>FindTargrt</source>
+        <translation>查找目标</translation>
+    </message>
+    <message>
+        <location filename="propertysettingwnd.cpp" line="48"/>
+        <source>Cpatured files folder</source>
+        <translation>捕获目标</translation>
+    </message>
+    <message>
+        <location filename="propertysettingwnd.cpp" line="59"/>
+        <location filename="propertysettingwnd.cpp" line="63"/>
+        <location filename="propertysettingwnd.cpp" line="269"/>
+        <location filename="propertysettingwnd.cpp" line="270"/>
+        <location filename="propertysettingwnd.cpp" line="273"/>
+        <location filename="propertysettingwnd.cpp" line="274"/>
+        <source>Language</source>
+        <translation>语言</translation>
+    </message>
+    <message>
+        <location filename="propertysettingwnd.cpp" line="70"/>
+        <location filename="propertysettingwnd.cpp" line="278"/>
+        <location filename="propertysettingwnd.cpp" line="279"/>
+        <source>Auto Update</source>
+        <translation>自动更新</translation>
+    </message>
+    <message>
+        <location filename="propertysettingwnd.cpp" line="81"/>
+        <source>GPU</source>
+        <translation>GPU</translation>
+    </message>
+    <message>
+        <location filename="propertysettingwnd.cpp" line="85"/>
+        <source>Enable GPU Acceleration</source>
+        <translation>允许使用GPU加速</translation>
+    </message>
+    <message>
+        <location filename="propertysettingwnd.cpp" line="95"/>
+        <location filename="propertysettingwnd.cpp" line="294"/>
+        <source>Learn more...</source>
+        <translation>了解更多</translation>
+    </message>
+    <message>
+        <location filename="propertysettingwnd.cpp" line="99"/>
+        <location filename="propertysettingwnd.cpp" line="297"/>
+        <location filename="propertysettingwnd.cpp" line="298"/>
+        <source>Message Center</source>
+        <translation>消息中心</translation>
+    </message>
+    <message>
+        <location filename="propertysettingwnd.cpp" line="103"/>
+        <source>Don&apos;t show MessageBox automaticaly</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="propertysettingwnd.cpp" line="110"/>
+        <location filename="propertysettingwnd.cpp" line="118"/>
+        <location filename="propertysettingwnd.cpp" line="305"/>
+        <location filename="propertysettingwnd.cpp" line="306"/>
+        <source>Reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="propertysettingwnd.cpp" line="114"/>
+        <source>Show all the notifacation messagees</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="propertysettingwnd.cpp" line="122"/>
+        <source>Default</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="propertysettingwnd.cpp" line="126"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="propertysettingwnd.cpp" line="130"/>
+        <source>Cancle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="propertysettingwnd.cpp" line="261"/>
+        <location filename="propertysettingwnd.cpp" line="262"/>
+        <source>Captured files folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="propertysettingwnd.cpp" line="281"/>
+        <location filename="propertysettingwnd.cpp" line="282"/>
+        <source>Check for upgrades automatically:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="propertysettingwnd.cpp" line="286"/>
+        <location filename="propertysettingwnd.cpp" line="287"/>
+        <source>Gpu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="propertysettingwnd.cpp" line="289"/>
+        <location filename="propertysettingwnd.cpp" line="290"/>
+        <source>Enable Gpu Acceleration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="propertysettingwnd.cpp" line="293"/>
+        <source>learn more...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="propertysettingwnd.cpp" line="300"/>
+        <location filename="propertysettingwnd.cpp" line="301"/>
+        <source>Don&apos;t show Messagees automaticaly</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="propertysettingwnd.cpp" line="309"/>
+        <location filename="propertysettingwnd.cpp" line="310"/>
+        <source>Show me all the notifacation messages</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SelectWindowSkin</name>
+    <message>
+        <location filename="selectwindowskin.cpp" line="28"/>
+        <source>E:\vs_programeFile\cplusplus\qt_study\dragAndDrop\drag\MyFimora\Resources\css</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingTitle</name>
+    <message>
+        <location filename="settingtitle.cpp" line="23"/>
+        <source>settingWndTitle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingtitle.cpp" line="24"/>
+        <source>Setting</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
